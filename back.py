39b0ce5deb8df47d273f12fc3e2d2b1786447701
vladimir <o.volodya@gmail.com>
2020-02-10 14:27:40 +0300

    Голобальный график

--- back.py
+++ back.py
@@ -2,7 +2,7 @@ import random
 points = []
 pointsValue = []
 date = []
-n = random.randint(4, 7)
+n = 6
 a = ''
 
 for i in range(n):
@@ -28,11 +28,11 @@ def add_date():
                 date.append(str(1 + w * 7) + ' . ' + str(m) + ' . ' + str(2019 + y))
 
 
-def add_points():
+def add_points(tud):
     for i in range(n):
         points[i][0] = '00' + str(i+1)
-        points[i][1][0] = str(random.randint(11, 100)) + '.' + str(random.randint(1000000, 10000000))
-        points[i][1][1] = str(random.randint(11, 100)) + '.' + str(random.randint(1000000, 10000000))
+        points[i][1][0] = tud[0][i]
+        points[i][1][1] = tud[1][i]
 
 
 def add_points_value():
@@ -43,7 +43,7 @@ def add_points_value():
             for h in range(3):
                 pointsValue[i][1][j][1][0] = random.randint(60, 80)
                 pointsValue[i][1][j][1][1] = random.randint(22, 27)
-                pointsValue[i][1][j][1][2] = random.random() + random.randint(0, 10)
+                pointsValue[i][1][j][1][2] = round(random.random(), 2) + random.randint(0, 10)
 
 
 def get_measurements_by_id(ID):
@@ -56,6 +56,27 @@ def get_measurements_by_id(ID):
     return pointsValue[t][1], ID
 
 
+def get_mid_value():
+    mid = []
+    date0 = []
+    for i in range(30):
+        mid.append([])
+        for j in range(3):
+            mid[i].append(0)
+    for i in range(n):
+        for j in range(30):
+            date0.append(j)
+            for h in range(3):
+                mid[j][h] += pointsValue[i][1][j][1][h]
+    for i in range(30):
+        for j in range(3):
+            if j != 2:
+                mid[i][j] = round(mid[i][j]/n)
+            else:
+                mid[i][j] = round(mid[i][j]/n, 2)
+    return mid, date0
+
+
 def get_points():
     return points, n
 
@@ -64,7 +85,15 @@ def get_date():
     return date
 
 
+def get_tud():
+    longitudes = [38.487501, 38.487501, 38.482901, 38.477501, 38.482901, 38.487501]
+    latitudes = [54.786754, 54.790001, 54.790501, 54.793905, 54.793905, 54.793905]
+    return longitudes, latitudes
+
+
 add_date()
-add_points()
+add_points(get_tud())
 add_points_value()
+get_mid_value()
+
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import io
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import back
 
 app = Flask(__name__)
@@ -88,5 +89,67 @@ def create_figure(ID):
     axis.plot(xs, yh, "-b", label="Влажность")
     axis.plot(xs, yt, "-r", label="Температура")
     axis.plot(xs, ya, "-g", label="Кислотность")
+    plt.xticks(xs, " ")
     axis.legend(loc="upper left")
+    return fig
+
+
+@app.route('/glob_graphic')
+def glob_graphic():
+    return render_template('glob_graphic.html')
+
+
+@app.route('/plot_mid.png')
+def plot_mid_png():
+    fig = create_mid_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_mid_figure():
+    listPointsValue = back.get_mid_value()
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = []
+    yh = []
+    yt = []
+    ya = []
+    for i in range(30):
+        xs.append(listPointsValue[1][i])
+        yh.append(listPointsValue[0][i][0])
+        yt.append(listPointsValue[0][i][1])
+        ya.append(listPointsValue[0][i][2])
+    axis.plot(xs, yh, "-b", label="Влажность")
+    axis.plot(xs, yt, "-r", label="Температура")
+    axis.plot(xs, ya, "-g", label="Кислотность")
+    plt.xticks(xs, " ")
+    axis.legend(loc="upper left")
+    return fig
+
+
+@app.route('/field.png')
+def field_png():
+    fig = create_field_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_field_figure():
+    BBox = (38.473225, 38.490792, 54.785954, 54.795505)
+    tud = back.get_tud()
+    longitudes = tud[0]
+    latitudes = tud[1]
+    fig, ax = plt.subplots(figsize=(12, 6))
+    field_img = plt.imread('./static/field.png')
+    ax.scatter(longitudes, latitudes, zorder=1, marker='^', alpha=1, c='r', s=50)
+ #   ax.set_title('Plotting Spatial Data on Riyadh Map')
+    ax.set_xlim(BBox[0], BBox[1])
+    ax.set_ylim(BBox[2], BBox[3])
+    cur_axes = plt.gca()
+    cur_axes.axes.get_xaxis().set_visible(False)
+    cur_axes.axes.get_yaxis().set_visible(False)
+    ax.imshow(field_img, zorder=0, extent=BBox, aspect='equal')
+
     return fig
